38879b82e6d5c4419144e35193e18252c85d7a9b
CoderLeechou <Lizhenweiwork@163.com>
2018-08-23 14:24:52 +0800

    将CrimeListActivity设为启动

--- CriminalIntent/app/src/main/java/com/lzwap/android/criminalintent/CrimeListActivity.java
+++ CriminalIntent/app/src/main/java/com/lzwap/android/criminalintent/CrimeListActivity.java
@@ -2,11 +2,9 @@ package com.lzwap.android.criminalintent;
 
 import android.support.v4.app.Fragment;
 
-
-public class CrimeActivity extends SingleFragmentActivity {
-
+public class CrimeListActivity extends SingleFragmentActivity {
     @Override
     protected Fragment createFragment() {
-        return new CrimeFragment();
+        return new CrimeListFragment();
     }
 }
---
+++ CriminalIntent/app/src/main/java/com/lzwap/android/criminalintent/CrimeListFragment.java
@@ -0,0 +1,6 @@
+package com.lzwap.android.criminalintent;
+
+import android.support.v4.app.Fragment;
+
+public class CrimeListFragment extends Fragment {
+}
