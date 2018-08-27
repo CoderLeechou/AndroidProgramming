065c9a6f311eb023b9200c28f755881bfb5c5bc5
CoderLeechou <Lizhenweiwork@163.com>
2018-08-27 16:52:50 +0800

    设置CrimePagerActivity中ViewPager的pager adpter

--- CriminalIntent/app/src/main/java/com/lzwap/android/criminalintent/CrimePagerActivity.java
+++ CriminalIntent/app/src/main/java/com/lzwap/android/criminalintent/CrimePagerActivity.java
@@ -2,13 +2,39 @@ package com.lzwap.android.criminalintent;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 
+import java.util.List;
+
 public class CrimePagerActivity extends AppCompatActivity {
+
+    private ViewPager mViewPager;
+    private List<Crime> mCrimes;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_crime_pager);
+
+        mViewPager = (ViewPager) findViewById(R.id.activity_crime_pager_view_pager);
+
+        mCrimes = CrimeLab.get(this).getCrimes();
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        mViewPager.setAdapter(new FragmentStatePagerAdapter(fragmentManager) {
+            @Override
+            public Fragment getItem(int i) {
+                Crime crime = mCrimes.get(i);
+                return CrimeFragment.newInstance(crime.getId());
+            }
+
+            @Override
+            public int getCount() {
+                return mCrimes.size();
+            }
+        });
     }
 }
