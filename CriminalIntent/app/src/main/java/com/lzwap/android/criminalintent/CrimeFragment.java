c8fad0eba89d4c619efe152891cdeab1a040636b
CoderLeechou <Lizhenweiwork@163.com>
2018-09-05 13:40:59 +0800

    对拍摄保存的照片进行缩放并显示

--- CriminalIntent/app/src/main/java/com/lzwap/android/criminalintent/CrimeFragment.java
+++ CriminalIntent/app/src/main/java/com/lzwap/android/criminalintent/CrimeFragment.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
 import android.database.Cursor;
+import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.ContactsContract;
@@ -239,6 +240,7 @@ public class CrimeFragment extends Fragment {
         });
 
         mPhotoView = (ImageView) v.findViewById(R.id.crime_photo);
+        updatePhotoView();
 
         return v;
     }
@@ -295,6 +297,11 @@ public class CrimeFragment extends Fragment {
                         c.close();
                     }
                 }
+            case REQUEST_PHOTO:
+                Uri uri = FileProvider.getUriForFile(getActivity(),
+                        "com.lzwap.android.criminalintent.fileprovider", mPhotoFile);
+                getActivity().revokeUriPermission(uri, Intent.FLAG_GRANT_WRITE_URI_PERMISSION);
+                updatePhotoView();
             default:
                 break;
         }
@@ -335,6 +342,15 @@ public class CrimeFragment extends Fragment {
                 solvedString, suspect);
 
         return report;
+    }
+
+    private void updatePhotoView() {
+        if (mPhotoFile == null || !mPhotoFile.exists()) {
+            mPhotoView.setImageDrawable(null);
+        } else {
+            Bitmap bitmap = PictureUtils.getScaledBitmap(mPhotoFile.getPath(), getActivity());
+            mPhotoView.setImageBitmap(bitmap);
+        }
     }
 
     public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
---
+++ CriminalIntent/app/src/main/java/com/lzwap/android/criminalintent/PictureUtils.java
@@ -0,0 +1,43 @@
+package com.lzwap.android.criminalintent;
+
+import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Point;
+
+public class PictureUtils {
+
+    //先确认屏幕的尺寸，然后按此缩放图像
+    public static Bitmap getScaledBitmap(String path, Activity activity) {
+        Point size = new Point();
+        activity.getWindowManager().getDefaultDisplay()
+                .getSize(size);
+
+        return getScaledBitmap(path, size.x, size.y);
+    }
+
+    //对拍摄的照片进行缩放以便Bitmap显示
+    public static Bitmap getScaledBitmap(String path, int destWidth, int destHeight) {
+        //读取照片像素大小
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inJustDecodeBounds = true;
+        BitmapFactory.decodeFile(path, options);
+
+        float srcWidth = options.outWidth;
+        float srcHeight = options.outHeight;
+
+        //缩略比，若为1，缩略图像素和原始照片水平像素大小一样
+        int inSampleSize = 1;
+        if (srcHeight > destHeight || srcWidth > destWidth) {
+            float heightScale = srcHeight / destHeight;
+            float widthScale = srcWidth / destWidth;
+
+            inSampleSize = Math.round(heightScale > widthScale ? heightScale : widthScale);
+        }
+
+        options = new BitmapFactory.Options();
+        options.inSampleSize = inSampleSize;
+
+        return BitmapFactory.decodeFile(path, options);
+    }
+}
