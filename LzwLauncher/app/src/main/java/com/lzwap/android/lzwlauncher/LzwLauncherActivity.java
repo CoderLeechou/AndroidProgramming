86934804ac7fe4f9862ce7e2bc95da0a32e23dee
CoderLeechou <Lizhenweiwork@163.com>
2018-09-10 15:12:33 +0800

    XMLDrawable实践，新建LzwLauncher

---
+++ LzwLauncher/app/src/main/java/com/lzwap/android/lzwlauncher/LzwLauncherActivity.java
@@ -0,0 +1,13 @@
+package com.lzwap.android.lzwlauncher;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LzwLauncherActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lzw_launcher);
+    }
+}
