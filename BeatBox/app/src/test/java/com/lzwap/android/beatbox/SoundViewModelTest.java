11aacae9cf0e211e5f2f5c487993400eecc6a547
CoderLeechou <Lizhenweiwork@163.com>
2018-09-10 09:30:15 +0800

    创建SoundViewModelTest单元测试

---
+++ BeatBox/app/src/test/java/com/lzwap/android/beatbox/SoundViewModelTest.java
@@ -0,0 +1,37 @@
+package com.lzwap.android.beatbox;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+
+public class SoundViewModelTest {
+
+    private BeatBox mBeatBox;
+    private Sound mSound;
+    private SoundViewModel mSubject;
+
+    @Before
+    public void setUp() throws Exception {
+        //用Mockito创建虚拟BeatBox对象
+        mBeatBox = mock(BeatBox.class);
+        mSound = new Sound("assetPath");
+        mSubject = new SoundViewModel(mBeatBox);
+        mSubject.setSound(mSound);
+    }
+
+    @Test
+    public void exposesSoundNameAsTitle() {
+        assertThat(mSubject.getTitle(), is(mSound.getName()));
+    }
+
+    @Test
+    public void callsBeatBoxPlayOnButtonClicked() {
+        mSubject.onButtonClicked();
+
+        verify(mBeatBox).play(mSound);
+    }
+}
