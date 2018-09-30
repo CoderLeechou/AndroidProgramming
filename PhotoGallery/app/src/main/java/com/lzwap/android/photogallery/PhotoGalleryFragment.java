ed4f2b084ddbfda904682b39dd084652a3614c18
CoderLeechou <Lizhenweiwork@163.com>
2018-09-30 14:57:26 +0800

    提交搜索，隐藏键盘

--- PhotoGallery/app/src/main/java/com/lzwap/android/photogallery/PhotoGalleryFragment.java
+++ PhotoGallery/app/src/main/java/com/lzwap/android/photogallery/PhotoGalleryFragment.java
@@ -1,5 +1,6 @@
 package com.lzwap.android.photogallery;
 
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.drawable.BitmapDrawable;
@@ -21,6 +22,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewTreeObserver;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -129,6 +131,16 @@ public class PhotoGalleryFragment extends Fragment {
                 QueryPreference.setStoredQuery(getActivity(), s);
                 mNextPage = 1;
                 updateItems(mNextPage);
+                if (searchView != null) {
+                    // 得到输入管理对象
+                    InputMethodManager imm = (InputMethodManager) getActivity()
+                            .getSystemService(Context.INPUT_METHOD_SERVICE);
+                    if (imm != null) {
+                        // 这将让键盘在所有的情况下都被隐藏，但是一般我们在点击搜索按钮后，输入法都会乖乖的自动隐藏的。
+                        imm.hideSoftInputFromWindow(searchView.getWindowToken(), 0); // 输入法如果是显示状态，那么就隐藏输入法
+                    }
+                    searchView.onActionViewCollapsed(); // 不获取焦点
+                }
                 return true;
             }
 
