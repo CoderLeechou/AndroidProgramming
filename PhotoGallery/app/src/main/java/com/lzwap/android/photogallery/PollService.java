fdd9d12afc646162c6a786b7bc1d66b969162892
CoderLeechou <Lizhenweiwork@163.com>
2018-10-09 10:44:17 +0800

    启动后台服务时，在状态栏显示Notification通知

--- PhotoGallery/app/src/main/java/com/lzwap/android/photogallery/PollService.java
+++ PhotoGallery/app/src/main/java/com/lzwap/android/photogallery/PollService.java
@@ -2,13 +2,17 @@ package com.lzwap.android.photogallery;
 
 import android.app.AlarmManager;
 import android.app.IntentService;
+import android.app.Notification;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.res.Resources;
 import android.net.ConnectivityManager;
 import android.os.SystemClock;
 import android.support.annotation.Nullable;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationManagerCompat;
 import android.util.Log;
 
 import java.util.List;
@@ -18,7 +22,7 @@ public class PollService extends IntentService {
     private static final String TAG = "PollService";
 
     //设置后台服务定时启动间隔
-    private static final long POLL_INTERVAL_MS = TimeUnit.MINUTES.toMillis(1);
+    private static final long POLL_INTERVAL_MS = TimeUnit.MINUTES.toMillis(15);
 
     public static Intent newIntent(Context context) {
         return new Intent(context, PollService.class);
@@ -80,6 +84,24 @@ public class PollService extends IntentService {
             Log.i(TAG, "Got an old result: " + resultId);
         } else {
             Log.i(TAG, "Got a new result: " + resultId);
+
+            //添加通知信息
+            Resources resources = getResources();
+            Intent i = PhotoGalleryActivity.newIntent(this);
+            PendingIntent pi = PendingIntent.getActivity(this, 0, i, 0);
+
+            Notification notification = new NotificationCompat.Builder(this)
+                    .setTicker(resources.getString(R.string.new_pictures_title))
+                    .setSmallIcon(android.R.drawable.ic_menu_report_image)
+                    .setContentTitle(resources.getString(R.string.new_pictures_title))
+                    .setContentText(resources.getString(R.string.new_pictures_text))
+                    .setContentIntent(pi)
+                    .setAutoCancel(true)
+                    .build();
+
+            NotificationManagerCompat notificationManagerCompat =
+                    NotificationManagerCompat.from(this);
+            notificationManagerCompat.notify(0, notification);
         }
 
         QueryPreference.setLastResultId(this, resultId);
